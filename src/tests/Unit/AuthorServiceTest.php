8262234c3fdf08de41ded83a10c28115f6f66fd7
PriorAl <alberto.prior@alc.co.jp>
2025-05-21 15:00:48 +0900

    test: Add unit tests for AuthorService

---
+++ src/tests/Unit/AuthorServiceTest.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use App\Services\AuthorService;
+use App\Author;
+use App\Book;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class AuthorServiceTest extends TestCase
+{
+    use RefreshDatabase;
+
+    protected AuthorService $service;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->service = app(AuthorService::class);
+    }
+
+    /** @test */
+    public function given_author_with_no_books_when_deleteIfOrphaned_then_author_is_deleted()
+    {
+        // Arrange
+        $author = Author::create(['name' => 'Orphan']);
+
+        // Act
+        $this->service->deleteIfOrphaned($author->id);
+
+        // Assert
+        $this->assertDatabaseMissing('authors', ['id' => $author->id]);
+    }
+
+    /** @test */
+    public function given_author_with_books_when_deleteIfOrphaned_then_author_is_not_deleted()
+    {
+        // Arrange
+        $author = Author::create(['name' => 'Writer']);
+        Book::create([
+            'title' => 'Book One',
+            'author_id' => $author->id,
+        ]);
+
+        // Act
+        $this->service->deleteIfOrphaned($author->id);
+
+        // Assert
+        $this->assertDatabaseHas('authors', ['id' => $author->id]);
+    }
+}
