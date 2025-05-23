a6b967897b8632d38991cb60a8b802f14d5e4373
PriorAl <alberto.prior@alc.co.jp>
2025-05-24 07:23:12 +0900

    Style: Implement tool bar style adding also new SVG icons for clear content.

--- src/resources/views/livewire/tool-bar.blade.php
+++ src/resources/views/livewire/tool-bar.blade.php
@@ -1,13 +1,20 @@
-<div style="margin: 10px 0; display: flex; gap: 10px;">
-    <input type="text" wire:model="search" placeholder="Search by title or author" />
+<div class="tool-bar">
+    <div class="search-container">
+        <input type="text" wire:model="search" placeholder="Search by title or author" class="search-input"/>
+        <button wire:click="$set('search', '')" class="clear-search" data-has-content="{{ $search ? 'true' : 'false' }}" title="{{ $search ? 'Clear search' : 'Search' }}">
+            <span class="icon-btn">{!! $search ? svg_icon('Close') : svg_icon('Search') !!}</span>
+        </button>
+    </div>
+    
+    <div class="buttons-container">
+        <button wire:click="sortBy('title')" class="btn-black">
+            Sort by Title ({{ $this->sortLabel('title') }})
+        </button>
 
-    <button wire:click="sortBy('title')">
-        Sort by Title ({{ $this->sortLabel('title') }})
-    </button>
+        <button wire:click="sortBy('author')" class="btn-black">
+            Sort by Author ({{ $this->sortLabel('author') }})
+        </button>
 
-    <button wire:click="sortBy('author')">
-        Sort by Author ({{ $this->sortLabel('author') }})
-    </button>
-
-    <button wire:click="resetSort">Reset</button>
+        <button wire:click="resetSort" class="btn-black">Reset</button>
+    </div>
 </div>
